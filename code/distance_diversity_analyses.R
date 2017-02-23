097a5396d5f735c5947cd210a747b5e42791bca1
eric-pedersen <eric.j.pedersen@gmail.com>
2017-02-22 23:48:37 -0600

    Added alpha diversity (based on the exponent of Shannon div) to figure S4

--- code/distance_diversity_analyses.R
+++ code/distance_diversity_analyses.R
@@ -5,6 +5,7 @@ rm(list=ls(all=TRUE))
 library(plyr)
 library(reshape2)
 library(dplyr)
+library(tidyr)
 library(vegan)
 library(bootstrap)
 library(RColorBrewer)
@@ -25,9 +26,9 @@ include_mean = T
 min_years_in_group = 30 
 #specifies how many years a polygon has to have observations in it for it to be used in the analysis
 
-top4_sp = c("GADUS_MORHUA","SEBASTES_MENTELLA",
+top4_sp_names = c("GADUS_MORHUA","SEBASTES_MENTELLA",
             "REINHARDTIUS_HIPPOGLOSSOIDES", "HIPPOGLOSSOIDES_PLATESSOIDES")
-top4_sp = match(top4_sp, names(voronoi_data))
+top4_sp = match(top4_sp_names, names(voronoi_data))
 nontop_sp = (8:37)[!(8:37)%in%top4_sp]
 
 
@@ -79,7 +80,24 @@ if(!include_variance){
 }
 
 
-
+# Calculating average diversity ####
+mean_diversity_data = voronoi_data %>%
+  select(Year,polygon, ANARHICHAS_DENTICULATUS:UROPHYCIS_TENUIS)%>%
+  gather(species,abundance,-Year,-polygon)%>%
+  group_by(Year,polygon) %>%
+  mutate(prop = abundance/sum(abundance),
+         abundance_rare = ifelse(species%in%top4_sp_names, 
+                                 NA, abundance),
+         prop_rare = abundance_rare/sum(abundance_rare,na.rm = T))%>%
+  summarise(diversity= sum(-prop*ifelse(prop==0, 0, log(prop))),
+            diversity= exp(diversity),
+            diversity_rare = sum(-prop_rare*ifelse(prop_rare==0, 0, log(prop_rare)),na.rm = T),
+            diversity_rare = exp(diversity_rare))%>%
+  group_by(Year)%>%
+  summarise(div_sd = sd(diversity,na.rm = T)/sqrt(n()), 
+            div_mean = mean(diversity,na.rm = T),
+            div_rare_sd = sd(diversity_rare, na.rm=T)/sqrt(n()-4),
+            div_rare_mean = mean(diversity_rare, na.rm=T))
 
 #Figure 5####
 ComV<-c("sp_dist_scl","sp_nontop_dist_scl")
@@ -119,8 +137,24 @@ mtext("Year",side=1,outer=T,line=1,  adj=0.525,cex=1,padj=0)
 legend("topleft", "B",bty='n', cex=1.8, inset=c(-0.03,-0.025))
 dev.off()
 
-png("Figures/Fig. S4.png", height=6,width=6,units="in",res = 800 )
-par(mfrow=c(2,1),mar=c(2,5,0,2),oma=c(3,1,1,1),las=1)
+png("Figures/Fig. S4.png", height=9,width=6,units="in",res = 800 )
+par(mfrow=c(3,1),mar=c(2,5,0,2),oma=c(3,1,1,1),las=1)
+plot(c(1981:2013),mean_diversity_data$div_mean,type='n',xaxt='n',ylim = c(2.5,4.75), 
+     ylab="Average diversity\n(effective number of species)", 
+     cex.lab=label.V,cex.axis=axis.V,xlab="", xlim=c(1980,2013))
+plotCI(c(1981:2013),mean_diversity_data$div_mean,
+       uiw=mean_diversity_data$div_sd,pch=NA,slty=1,sfrac=0,add=T)
+lines(c(1981:1994),mean_diversity_data$div_mean[1:14], lty=1, lwd=2)
+lines(c(1995:2013),mean_diversity_data$div_mean[15:33], lty=1, lwd=2)
+
+plotCI(c(1981:2013),mean_diversity_data$div_rare_mean,
+       uiw=mean_diversity_data$div_rare_sd,pch=NA,slty=2,sfrac=0,add=T)
+lines(c(1981:1994),mean_diversity_data$div_rare_mean[1:14], lty=2, lwd=2)
+lines(c(1995:2013),mean_diversity_data$div_rare_mean[15:33], lty=2, lwd=2)
+legend("topleft", "A",bty='n', cex=1.8, inset=c(-0.03,-0.025))
+abline(v=Eras, lwd=1,lty=2, col=8)
+
+
 plot(c(1981:2013),div_dist_models[div_dist_models$community_subset=="sp_dist_scl" & div_dist_models$coef=="Mean community distance","R2_value"],type='n',ylim = c(0.3,2.4), ylab="mean dissimilarity", cex.lab=label.V,cex.axis=axis.V, xlim=c(1980,2013),xaxt='n',xlab="")
 for(i in 1:3){
   plotCI(c(1981:2013),div_dist_models[div_dist_models$community_subset==ComV[i] & div_dist_models$coef=="Mean community distance","R2_value"],uiw=div_dist_models[div_dist_models$community_subset==ComV[i] & div_dist_models$coef=="Mean community distance","se"],pch=NA,slty=i,sfrac=0,add=T)
@@ -129,7 +163,7 @@ for(i in 1:3){
 }
 abline(v=Eras, lwd=1,lty=2, col=8)
 mtext("Year",side=1,outer=T,line=1,  adj=0.525,cex=1,padj=0)
-legend("topleft", "A",bty='n', cex=1.8, inset=c(-0.03,-0.025))
+legend("topleft", "B",bty='n', cex=1.8, inset=c(-0.03,-0.025))
 
 
 plot(c(1981:2013),div_dist_models[div_dist_models$community_subset=="sp_dist_scl" & div_dist_models$coef=="variance","R2_value"],type='n',ylim = c(0.9,4.55), ylab="total variance", cex.lab=label.V,cex.axis=axis.V,xlab="", xlim=c(1980,2013))
@@ -140,7 +174,7 @@ for(i in 1:3){
 }
 abline(v=Eras, lwd=1,lty=2, col=8)
 mtext("Year",side=1,outer=T,line=1,  adj=0.525,cex=1,padj=0)
-legend("topleft", "B",bty='n', cex=1.8, inset=c(-0.03,-0.025))
+legend("topleft", "C",bty='n', cex=1.8, inset=c(-0.03,-0.025))
 legend(x="topright", legend=c("Total community","Noncommercial species"),
        col="black", lty=1:3,lwd=3,bty='n',box.col="#FF003300", cex=0.9)
 
